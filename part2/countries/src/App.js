ad4b368915c4718b683629b0060fc1b362adfe52
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2021-07-12 13:17:00 -0400

    Fix country content

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -41,7 +41,7 @@ const Country = ({country}) => {
       <h2>Weather in {country.capital}</h2>
       <div><b>Temperature:</b> {weather.temperature} Celsius</div>
       <img src={weather.weather_icons} alt={weather.weather_descriptions} />
-      <div><b>Wind:</b> {weather.wind_speed} direction {weather.wind_dir}</div>
+      <div><b>Wind:</b> {weather.wind_speed} mph direction {weather.wind_dir}</div>
     </div>
   )
 }
